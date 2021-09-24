0779a9572d528ba0fc8694d09fa0025bf58764bf
shangus1012 <91159096+shangus1012@users.noreply.github.com>
2021-09-24 23:41:18 +0900

    increase loading speed

--- js/copy.js
+++ js/copy.js
@@ -1,7 +1,7 @@
 function copyToClipboard(i) { //copy 함수
     const t = document.createElement("textarea");
     document.body.appendChild(t);
-    t.value = deleteETC(arr[i]);
+    t.value = deleteETC(arrT[i]);
     t.select();
     document.execCommand('copy');
     document.body.removeChild(t);
@@ -12,3 +12,11 @@ function deleteETC(str) { //복사할 텍스트에서 설명문을 지우기 위
     alert("복사됨 [" + temp[0] + "]");
     return temp[0];
 }
+
+
+/*
+    source : https://github.com/shangus1012/manga
+    제작자 : 만갤 여주학대만화추천(rmh0102)
+    모든 소스코드는 직접 작성한 것이고, 출처는 지워도 상관 없습니다.
+    누군가 물어본다면 정보 공유를 위해서 출처를 표기해 주세요. 감사합니다.
+*/
--- js/makeT.js
+++ js/makeT.js
@@ -3,27 +3,30 @@ var strB = "";
 var strC = "";
 var strD = "";
 var strAll = "";
-var arr;
+var arrA = strA.split("/");
+var arrB = strB.split("/");
+var arrC = strC.split("/");
+var arrD = strD.split("/");
+var arrAll = strAll.split("/");
+var arrT;
 
-function setup(n) { //탭에 맞는 list를 set하기 위한 함수
+function setup(n) { //탭에 맞는 list를 불러오기 위한 함수
     var temp;
     if (n == 1) {
-        temp = strAll;
+        maketable(arrAll);
     }
     else if(n == 2) {
-        temp = strA;
+        maketable(arrA);
     }
     else if (n == 3) {
-        temp = strB;
+        maketable(arrB);
     }
     else if (n == 4) {
-        temp = strC;
+        maketable(arrC);
     }
     else if (n == 5) {
-        temp = strD;
+        maketable(arrD);
     }
-    arr = temp.split("/");
-    maketable(arr); //list set 후 테이블 생성
 }
 
 function maketable(arr) { // 테이블을 생성하는 함수
@@ -33,11 +36,12 @@ function maketable(arr) { // 테이블을 생성하는 함수
         var temp = "<tr onclick='copyToClipboard(" + i +");'><td>" + i + "</td>" + "<td>" + arr[i] + "</td></tr>";
         tbody.innerHTML += temp;
     }
+    arrT = arr;
 }
 
 /*
     source : https://github.com/shangus1012/manga
     제작자 : 만갤 여주학대만화추천(rmh0102)
     모든 소스코드는 직접 작성한 것이고, 출처는 지워도 상관 없습니다.
-    그러나 정보 공유를 위해 누군가 물어본다면 출처를 표기해 주세요. 감사합니다.
+    누군가 물어본다면 정보 공유를 위해서 출처를 표기해 주세요. 감사합니다.
 */
